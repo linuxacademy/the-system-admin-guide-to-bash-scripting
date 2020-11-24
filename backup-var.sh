22f70c11547820b26beed95ccb312153a15037eb
Rob Marti <rob@linuxacademy.com>
2020-11-24 15:38:36 -0600

    Adding variables lab

---
+++ backup-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MYLOG=$1 
+
+echo "Creating backup directory" >> /home/$USER/$MYLOG
+mkdir /home/$USER/work_backup
+
+echo "Copying Files" >> /home/$USER/$MYLOG
+cp -v /home/$USER/work/* /home/$USER/work_backup/ >> /home/$USER/$MYLOG
+echo "Finished Copying Files" >> /home/$USER/$MYLOG
